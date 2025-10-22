2dc6bdeb3c57f5929ddf8abd55c03b7ddf2dc864
danipad100 <danipad100@gmail.com>
2025-10-22 19:46:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const VERSION = 'v8';
+const VERSION = 'v9';
 const CACHE_NAME = 'subli-mada-v21-cache-' + VERSION;
 const ASSETS = [
   './',
@@ -12,7 +12,7 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  // wait for user confirmation to activate new SW
+  // Do not call skipWaiting() here – wait for user confirmation from the page
 });
 
 self.addEventListener('activate', (event) => {
